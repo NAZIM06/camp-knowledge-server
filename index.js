572037f62540da665c5c1066fca25081939d2296
unknown <anikm2982@gmail.com>
2023-06-10 12:36:37 +0600

    class db created

--- index.js
+++ index.js
@@ -28,67 +28,73 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await
-     client.connect();
-// db and Collection
-
-  
-const userCollection = client.db('SummarCamp').collection('users')
+    client.connect();
+    // db and Collection
 
 
+    const userCollection = client.db('SummarCamp').collection('users')
+    const classCollection = client.db('SummarCamp').collection('Classes')
 
 
 
-      //users api
-      app.post('/all-users', async (req, res) => {
-        const user = req.body;
-        const query = { email: user?.email }
-        const existingUser = await userCollection.findOne(query);
-        if (existingUser) {
-            return res.send({})
-        }
-        const result = await userCollection.insertOne(user)
-        res.send(result)
+    //classes api
+    app.post('/all-classes', async (req, res) => {
+      const classes = req.body
+      const result = await classCollection.insertOne(classes)
+      res.send(result)
     })
-    app.get('/user/admin/:email', async(req, res)=>{
-        const email = req.params.email;
-        const query = {email : email, role : 'admin'}
-        console.log(email)
-        const admin = await userCollection.findOne(query)
-        res.send(admin)
+
+    //users api
+    app.post('/all-users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user?.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({})
+      }
+      const result = await userCollection.insertOne(user)
+      res.send(result)
     })
-    app.get('/user/instructor/:email', async(req, res)=>{
-        const email = req.params.email;
-        const query = {email : email, role : 'instructor'}
-        const instructor = await userCollection.findOne(query)
-        res.send(instructor)
+    app.get('/user/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email, role: 'admin' }
+      console.log(email)
+      const admin = await userCollection.findOne(query)
+      res.send(admin)
     })
-    app.get('/user/student/:email', async(req, res)=>{
-        const email = req.params.email;
-        const query = {email : email, role : 'student'}
-        const student = await userCollection.findOne(query)
-        res.send(student)
+    app.get('/user/instructor/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email, role: 'instructor' }
+      const instructor = await userCollection.findOne(query)
+      res.send(instructor)
+    })
+    app.get('/user/student/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email, role: 'student' }
+      const student = await userCollection.findOne(query)
+      res.send(student)
     })
     app.get('/current-user', async (req, res) => {
-        const email = req.query.email;
-        const query = { email: email }
-        const result = await userCollection.findOne(query)
-        res.send(result)
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await userCollection.findOne(query)
+      res.send(result)
     })
     app.get('/all-users', async (req, res) => {
-        const result = await userCollection.find().toArray()
-        res.send(result)
+      const result = await userCollection.find().toArray()
+      res.send(result)
     })
     app.put('/all-users/:id', async (req, res) => {
-        const id = req.params.id;
-        const role = req.query.role;
-        const query = { _id: new ObjectId(id) }
-        const updatedDoc = {
-            $set: {
-                role: role
-            }
+      const id = req.params.id;
+      const role = req.query.role;
+      const query = { _id: new ObjectId(id) }
+      const updatedDoc = {
+        $set: {
+          role: role
         }
-        const result = await userCollection.updateOne(query, updatedDoc)
-        res.send(result)
+      }
+      const result = await userCollection.updateOne(query, updatedDoc)
+      res.send(result)
     })
 
 
