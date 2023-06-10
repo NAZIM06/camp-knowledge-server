acb704aa0bb1f14108c27d111031644d6e162ebc
unknown <anikm2982@gmail.com>
2023-06-10 12:32:17 +0600

    user data base created

--- index.js
+++ index.js
@@ -4,16 +4,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
 require('dotenv').config()
-
-
-// const EventEmitter = require('events');
-// const bus = new EventEmitter();
-// const stripe = require('stripe')
-
-
+const EventEmitter = require('events');
+const stripe = require('stripe')
 
 // middleware
-
 app.use(cors())
 app.use(express.static("public"))
 app.use(express.json())
@@ -35,11 +29,67 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await
      client.connect();
-
-    // db and Collection
+// db and Collection
 
   
+const userCollection = client.db('SummarCamp').collection('users')
 
+
+
+
+
+      //users api
+      app.post('/all-users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user?.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+            return res.send({})
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+    app.get('/user/admin/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email : email, role : 'admin'}
+        console.log(email)
+        const admin = await userCollection.findOne(query)
+        res.send(admin)
+    })
+    app.get('/user/instructor/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email : email, role : 'instructor'}
+        const instructor = await userCollection.findOne(query)
+        res.send(instructor)
+    })
+    app.get('/user/student/:email', async(req, res)=>{
+        const email = req.params.email;
+        const query = {email : email, role : 'student'}
+        const student = await userCollection.findOne(query)
+        res.send(student)
+    })
+    app.get('/current-user', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email }
+        const result = await userCollection.findOne(query)
+        res.send(result)
+    })
+    app.get('/all-users', async (req, res) => {
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
+    app.put('/all-users/:id', async (req, res) => {
+        const id = req.params.id;
+        const role = req.query.role;
+        const query = { _id: new ObjectId(id) }
+        const updatedDoc = {
+            $set: {
+                role: role
+            }
+        }
+        const result = await userCollection.updateOne(query, updatedDoc)
+        res.send(result)
+    })
 
 
     // Send a ping to confirm a successful connection
