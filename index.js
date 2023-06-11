599230d49cf1b1f04d2a2723d8edd3c0cbd0a758
unknown <anikm2982@gmail.com>
2023-06-12 00:06:59 +0600

    vercel and jwt added

--- index.js
+++ index.js
@@ -12,7 +12,22 @@ app.use(cors())
 app.use(express.static("public"))
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'Unauthorize Access' })
+  }
+  const token = authorization.split(' ')[1];
 
+  jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      console.log(error)
+      return res.send({ error: true, message: 'Unauthorize Access' })
+    }
+    req.decoded = decoded;
+    next()
+  })
+}
 // uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmpua4z.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -36,15 +51,15 @@ async function run() {
     const classCollection = client.db('SummarCamp').collection('Classes')
     const selectedClassCollection = client.db('SummarCamp').collection('selectedClass')
     const enrolledClassCollection = client.db('SummarCamp').collection('enrolledClass')
-    const instructorsCollection = client.db('SummarCamp').collection('allInstructors')
     const paymentCollection = client.db('SummarCamp').collection('payment')
 
-
-    //instructor api
-    app.get('/instructors', async (req, res) => {
-      const result = await instructorsCollection.find().toArray()
-      res.send(result)
+    // JWT
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, { expiresIn: '10h' })
+      res.send({ token })
     })
+
 
     //classes api
     app.post('/all-classes', async (req, res) => {
@@ -57,7 +72,7 @@ async function run() {
       const result = await selectedClassCollection.insertOne(selectedClass)
       res.send(result)
     })
-    app.get('/selected-classes', async (req, res) => {
+    app.get('/selected-classes', verifyJWT, async (req, res) => {
       const email = req.query.email;
       const query = { studentEmail: email }
       const result = await selectedClassCollection.find(query).toArray()
@@ -69,18 +84,25 @@ async function run() {
       const result = await selectedClassCollection.findOne(query)
       res.send(result)
     })
-    app.delete('/selected-classes/:id', async (req, res) => {
+    app.delete('/selected-classes/:id', verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await selectedClassCollection.deleteOne(query)
       res.send(result)
     })
-    app.get('/all-classes', async (req, res) => {
-      const result = await classCollection.find().toArray()
+    app.get('/approved-all-classes', async (req, res) => {
+      const query = { status: 'Approved' } //only shows the approved classes
+      const result = await classCollection.find(query).toArray()
       res.send(result)
     })
-    app.get('/all-classes', async (req, res) => {
+    app.get('/all-classes', verifyJWT, async (req, res) => {
       const email = req.query.email;
+      const query = { instructorEmail: email }
+      const result = await classCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.get('/all-classes/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
       const query = { instructorEmail: email }
       const result = await classCollection.find(query).toArray()
       res.send(result)
@@ -120,7 +142,17 @@ async function run() {
         res.send(result)
       }
     })
-
+    app.get('/popular-classes', async (req, res) => {
+      const query = { status: 'Approved' }
+      const result = await classCollection.find(query).sort({ enrolledStudents: -1 }).toArray()
+      res.send(result)
+    })
+    app.get('/enrolled-classes/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email
+      const query = { studentEmail: email }
+      const result = await enrolledClassCollection.find(query).toArray()
+      res.send(result)
+    })
 
     //users api
     app.post('/all-users', async (req, res) => {
@@ -133,20 +165,20 @@ async function run() {
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
-    app.get('/user/admin/:email', async (req, res) => {
+    app.get('/user/admin/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
       const query = { email: email, role: 'admin' }
       console.log(email)
       const admin = await userCollection.findOne(query)
       res.send(admin)
     })
-    app.get('/user/instructor/:email', async (req, res) => {
+    app.get('/user/instructor/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
       const query = { email: email, role: 'instructor' }
       const instructor = await userCollection.findOne(query)
       res.send(instructor)
     })
-    app.get('/user/student/:email', async (req, res) => {
+    app.get('/user/student/:email', verifyJWT, async (req, res) => {
       const email = req.params.email;
       const query = { email: email, role: 'student' }
       const student = await userCollection.findOne(query)
@@ -158,11 +190,11 @@ async function run() {
       const result = await userCollection.findOne(query)
       res.send(result)
     })
-    app.get('/all-users', async (req, res) => {
+    app.get('/all-users', verifyJWT, async (req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
-    app.put('/all-users/:id', async (req, res) => {
+    app.put('/all-users/:id', verifyJWT, async (req, res) => {
       const id = req.params.id;
       const role = req.query.role;
       const query = { _id: new ObjectId(id) }
@@ -174,37 +206,46 @@ async function run() {
       const result = await userCollection.updateOne(query, updatedDoc)
       res.send(result)
     })
+    app.get('/all-instructors', async (req, res) => {
+      const query = { role: 'instructor' }
+      const result = await userCollection.find(query).toArray()
+      res.send(result)
+    })
+    app.get('/popular-instructors', async (req, res) => {
+      const query = { role: 'instructor' }
+      const result = await userCollection.find(query).toArray()
+      res.send(result)
+    })
 
-
-    //payment api
-    app.post("/create-payment-intent", async (req, res) => {
+     //payment api
+     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = Math.round(price * 100);
       const stripeClient = stripe(process.env.STRIPE_KEY);
       try {
-        // Create a PaymentIntent with the order amount and currency
-        const paymentIntent = await stripeClient.paymentIntents.create({
-          amount: amount,
-          currency: "usd",
-          automatic_payment_methods: {
-            enabled: true,
-          },
-        });
+          // Create a PaymentIntent with the order amount and currency
+          const paymentIntent = await stripeClient.paymentIntents.create({
+              amount: amount,
+              currency: "usd",
+              automatic_payment_methods: {
+                  enabled: true,
+              },
+          });
 
-        res.send({
-          clientSecret: paymentIntent.client_secret,
-        });
+          res.send({
+              clientSecret: paymentIntent.client_secret,
+          });
       } catch (error) {
-        console.log(error);
-        res.status(500).send({ error: "An error occurred while creating the PaymentIntent." });
+          console.log(error);
+          res.status(500).send({ error: "An error occurred while creating the PaymentIntent." });
       }
-    });
-    app.post('/payment', async (req, res) => {
+  });
+  app.post('/payment',verifyJWT, async (req, res) => {
       const payment = req.body;
       const insertResult = await paymentCollection.insertOne(payment);
 
       const enrolledQuery = { studentEmail: payment.email, classId: payment.classId }
-      const enrolledClass = await selectedClassCollection.findOne(enrolledQuery)
+      const enrolledClass =  await selectedClassCollection.findOne(enrolledQuery)
       const enrolledInsertResult = await enrolledClassCollection.insertOne(enrolledClass)
 
       const enrolledDeleteResult = await selectedClassCollection.deleteOne(enrolledQuery)
@@ -213,25 +254,23 @@ async function run() {
       const classDocument = await classCollection.findOne(classQuery); // Fetch the latest document
 
       if (classDocument && classDocument.seats > 0) {
-        const updatedSeats = classDocument.seats - 1; // Calculate the updated seats value
-        const updateResult = await classCollection.updateOne(
-          classQuery,
-          { $set: { seats: updatedSeats } }
-        );
+          const updatedSeats = classDocument.seats - 1;
+          const updateEnrolledStudents = classDocument.enrolledStudents + 1;
+          const updateResult = await classCollection.updateOne(
+              classQuery,
+              { $set: { seats: updatedSeats, enrolledStudents : updateEnrolledStudents } }
+          );
 
-        res.send({ insertResult, updateResult, enrolledDeleteResult, enrolledInsertResult });
+          res.send({ insertResult, updateResult, enrolledDeleteResult, enrolledInsertResult });
       }
-    });
-    app.get('/enrolled-classes', async (req, res) => {
-      const result = await enrolledClassCollection.find().toArray()
-      res.send(result)
-    })
-    app.get('/payment-history', async (req, res) => {
+  });
+  app.get('/payment-history',verifyJWT, async (req, res) => {
       const email = req.query.email;
-      const query = { email: email }
-      const result = await paymentCollection.find(query).toArray()
+      const query = {email : email}
+      const result = await paymentCollection.find(query).sort({date : -1}).toArray()
       res.send(result)
-    })
+  })
+
 
     // -----END-----
 
@@ -240,7 +279,8 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
+    // await 
+    client.close();
   }
 }
 run().catch(console.dir);
